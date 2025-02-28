17026504d8f0c9f48e3b17e021f6ed8090ad234e
SANDHYAGOPAL <86228328+SANDHYAGOPAL@users.noreply.github.com>
2025-02-28 15:27:01 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,8 +4,6 @@ print("Hi more local repo");
 print("Testing new branch");
 print("Jenkins Test Job");
 print("testing");
-<<<<<<< HEAD
 print("testing locally");
-=======
 print("testing remote");
->>>>>>> 51b1d054e55f0cfed6dfcd213b0c71b38ceb5607
+
